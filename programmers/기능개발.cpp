fef1f3d1a17d237861b1c49dc04eb1b25e1ba18c
cykei <kelle111@naver.com>
2022-04-09 11:50:27 +0900

    Update 기능개발.cpp

--- programmers/기능개발.cpp
+++ programmers/기능개발.cpp
@@ -1,6 +1,6 @@
 /*
 
-2020.02.14
+2022.04.09
 https://programmers.co.kr/learn/courses/30/lessons/42586
 
 한번 순회에 남은시간들 다 구해서 저장한다.
@@ -9,42 +9,29 @@ https://programmers.co.kr/learn/courses/30/lessons/42586
 
 #include <string>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
-    vector<int> answer;
-    int len = progresses.size();
-
-    vector<int> remain;
-    remain.resize(len);
-
-    int namugi, mok, time;
-    // 각 작업이 끝나는 날짜를 계산한다.
-    for (int i=0; i<len; i++){
-        namugi = (100-progresses[i])% speeds[i];
-        mok = (100-progresses[i])/ speeds[i];
-        time = namugi>0? mok+1 : mok;
-
-        remain[i] = time;
+	vector<int> answer;
+	vector<int> temp;
+	for (int i = 0; i < progresses.size(); i++) {
+		int time = (100-progresses[i]) / speeds[i];
+		if ((100-progresses[i]) % speeds[i] != 0) time++;
+		temp.push_back(time);
+	}
+    int cur = temp[0];
+    int cnt = 1;
+	for (int i=1; i<temp.size(); i++) {
+        if(temp[i] <= cur) cnt++;
+        else {
+            answer.push_back(cnt);
+            cur = temp[i];
+            cnt = 1;
+        }
     }
+    answer.push_back(cnt);
+    
 
-    int prev=remain[0];
-    int cur;
-    int result=1;
-    for (int i=1; i<len; i++){
-        cur = remain[i];
-        if (cur <= prev){
-            result++;
-        }else{
-            answer.push_back(result);
-            prev = cur; //기준을 변경한다.
-            result = 1;
-        }    
-    }
-
-    answer.push_back(result); //마지막을 넣는다. 
-
-
-    return answer;
+	return answer;
 }
